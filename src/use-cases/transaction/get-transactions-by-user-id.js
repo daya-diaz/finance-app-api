97490dcd8d6c32c0a8bde8def1ab3dedd8d04be2
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-01-24 19:37:40 -0300

    feat: add GetTransactionsByUserIdUseCase

---
+++ src/use-cases/transaction/get-transactions-by-user-id.js
@@ -0,0 +1,23 @@
+import { userNotFoundResponse } from '../../controllers/helpers/user.js'
+
+export class GetTransactionsByUserIdUseCase {
+    constructor(
+        getUserByIdRepository,
+        postgresGetTransactionsByUserIdRepository,
+    ) {
+        ;(this.getUserByIdRepository = getUserByIdRepository),
+            (this.postgresGetTransactionsByUserIdRepository =
+                postgresGetTransactionsByUserIdRepository)
+    }
+
+    async execute(userId) {
+        const userExists = await this.getUserByIdRepository.execute(userId)
+
+        if (!userExists) userNotFoundResponse()
+
+        const transactions =
+            await this.postgresGetTransactionsByUserIdRepository.execute(userId)
+
+        return transactions
+    }
+}
