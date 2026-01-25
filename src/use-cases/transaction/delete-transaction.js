0d6413e3eac0fe3a0fdca9761e1f48fcfdbf21cd
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-01-25 16:42:11 -0300

    feat: add DeleteTransactionUseCase

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,15 @@
+export class DeleteTransactionUseCase {
+    constructor(postgresDeleteTransactionRepository) {
+        this.postgresDeleteTransactionRepository =
+            postgresDeleteTransactionRepository
+    }
+
+    async execute(transactionId) {
+        const deletedTransaction =
+            await this.postgresDeleteTransactionRepository.execute(
+                transactionId,
+            )
+
+        return deletedTransaction
+    }
+}
