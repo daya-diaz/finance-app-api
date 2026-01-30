34da8ed3cfedfeed13e70e51207c4033eeda4735
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-01-30 08:00:33 -0300

    refactor: remove unnecessary code from CreateUserRepository

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -2,15 +2,8 @@ import prisma from '../../../../prisma/prisma.js'
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        const user = await prisma.user.create({
-            data: {
-                id: createUserParams.id,
-                first_name: createUserParams.first_name,
-                last_name: createUserParams.last_name,
-                email: createUserParams.email,
-                password: createUserParams.password,
-            },
+        return await prisma.user.create({
+            data: createUserParams,
         })
-        return user
     }
 }
