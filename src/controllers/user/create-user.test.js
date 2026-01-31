55c521370f339d11e0ab4bfc88f30b12242e366f
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-01-31 12:21:24 -0300

    test: ensure to return 400 if password is not valid

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -132,4 +132,25 @@ describe('CreateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 if password is invalid', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: 'John',
+                last_name: 'Doe',
+                email: 'johndoe@example.com',
+                password: '11',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
