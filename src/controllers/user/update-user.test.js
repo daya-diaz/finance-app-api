b12e2150032851e6240f7c1042d8ed4d5a05dc37
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-04 08:43:35 -0300

    test: ensure to return 400 when an unallowed field is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -87,4 +87,18 @@ describe('UpdateUserController', () => {
         // assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when an unallowed field is provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: { ...httpRequest.body, unallowed_field: 'unallowed_field' },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
