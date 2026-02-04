bdae34964f118166f9147d14a84a49c8f240cf5f
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-04 08:28:18 -0300

    test: ensure to return 400 when an invalid password is provided to update user

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -53,6 +53,24 @@ describe('UpdateUserController', () => {
             },
         })
 
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an invalid password is provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest,
+                password: '123',
+            },
+        })
+
+        // assert
         expect(result.statusCode).toBe(400)
     })
 })
