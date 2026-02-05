79e4f3a1202c1a2b5d395bbc1868d5844c7d8f28
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-05 08:13:47 -0300

    test: ensure to return 400 if CreateTransactionUseCase throws a ZodError

---
+++ src/controllers/transaction/create-transaction.test.js
@@ -0,0 +1,80 @@
+import { faker } from '@faker-js/faker'
+import { CreateTransactionController } from './create-transaction'
+import { ZodError } from 'zod'
+
+describe('CreateTransactionController', () => {
+    class CreateTransactionUseCaseStub {
+        async execute(transaction) {
+            return transaction
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionUseCase = new CreateTransactionUseCaseStub()
+        const sut = new CreateTransactionController(createTransactionUseCase)
+
+        return {
+            createTransactionUseCase,
+            sut,
+        }
+    }
+
+    const httpRequest = {
+        body: {
+            user_id: faker.string.uuid(),
+            name: faker.commerce.productName(),
+            date: faker.date.anytime().toISOString(),
+            type: 'EARNING',
+            amount: Number(faker.finance.amount()),
+        },
+    }
+
+    it('should return 201 when create a transaction successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(201)
+    })
+
+    it('should return 500 when CreateTransactionUseCase throws an error', async () => {
+        // arrange
+        const { sut, createTransactionUseCase } = makeSut()
+        jest.spyOn(createTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
+
+    it('should return 400 if CreateTransactionUseCase throws a ZodError', async () => {
+        // arrange
+        const { sut, createTransactionUseCase } = makeSut()
+        const zodError = new ZodError([
+            {
+                code: 'invalid_type',
+                expected: 'string',
+                received: 'number',
+                path: ['date'],
+                message: 'Expected string, received number',
+            },
+        ])
+        jest.spyOn(createTransactionUseCase, 'execute').mockRejectedValueOnce(
+            zodError,
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
+        expect(result.body.message).toBe('Expected string, received number')
+    })
+})
