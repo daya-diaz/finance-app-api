4adaf36d5dd5bb0aad30f9bb6a7014a26ec1f250
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-17 09:42:12 -0300

    test: create tests to UpdateTransactionController

---
+++ src/controllers/transaction/update-transaction.test.js
@@ -0,0 +1,146 @@
+import { ZodError } from 'zod'
+import { UpdateTransactionController } from './update-transaction'
+import { faker } from '@faker-js/faker'
+
+describe('UpdateTransactionController', () => {
+    class UpdateTransactionUseCaseStub {
+        async execute() {
+            return {
+                user_id: faker.string.uuid(),
+                id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EARNING',
+                amount: Number(faker.finance.amount()),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+        const sut = new UpdateTransactionController(updateTransactionUseCase)
+
+        return {
+            sut,
+            updateTransactionUseCase,
+        }
+    }
+
+    it('should return 200 when updating a transaction successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            params: {
+                transactionId: faker.string.uuid(),
+            },
+            body: {
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EARNING',
+                amount: Number(faker.finance.amount()),
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(200)
+    })
+
+    it('should return 400 if id is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            params: {
+                transactionId: 'invalid_transactionId',
+            },
+            body: {
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EARNING',
+                amount: Number(faker.finance.amount()),
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 400 if some field is not allowed to update', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            params: {
+                transactionId: faker.string.uuid(),
+            },
+            body: {
+                name: faker.commerce.productName(),
+                transactionId: faker.string.uuid(),
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+        expect(response.body.message).toBe(
+            'Some fields are not allowed to be updated.',
+        )
+    })
+
+    it('should return 400 if UpdateTransactionUseCase throws a ZodError', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        const zodError = new ZodError([
+            {
+                code: 'invalid_type',
+                expected: 'string',
+                received: 'number',
+                path: ['date'],
+                message: 'Expected string, received number',
+            },
+        ])
+        jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(
+            zodError,
+        )
+
+        // act
+        const response = await sut.execute({
+            params: {
+                transactionId: faker.string.uuid(),
+            },
+            body: {
+                date: faker.number.bigInt(),
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 500 when UpdateTransactionUseCase throws a server error', async () => {
+        // arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const response = await sut.execute({
+            params: {
+                transactionId: faker.string.uuid(),
+            },
+            body: {
+                name: faker.commerce.productName(),
+                date: faker.date.anytime().toISOString(),
+                type: 'EARNING',
+                amount: Number(faker.finance.amount()),
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(500)
+    })
+})
