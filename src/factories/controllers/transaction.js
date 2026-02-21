1af97d7f53e9c7de77a616ce645d1b1e18372241
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-21 14:20:23 -0300

    feat: ensure CreateTransactionUseCase uses IdGeneratorAdapter

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -1,3 +1,4 @@
+import { IdGeneratorAdapter } from '../../adapters/id-generator.js'
 import {
     CreateTransactionController,
     GetTransactionsByUserIdController,
@@ -22,9 +23,11 @@ export const makeCreateTransactionController = () => {
     const createTransactionRepository =
         new PostgresCreateTransactionRepository()
     const getUserByIdRepository = new PostgresGetUserByIdRepository()
+    const idGeneratorAdapter = new IdGeneratorAdapter()
     const createTransactionUseCase = new CreateTransactionUseCase(
         getUserByIdRepository,
         createTransactionRepository,
+        idGeneratorAdapter,
     )
     const createTransactionController = new CreateTransactionController(
         createTransactionUseCase,
--- src/use-cases/transaction/create-transaction.js
+++ src/use-cases/transaction/create-transaction.js
@@ -1,10 +1,14 @@
 import { UserNotFoundError } from '../../errors/user.js'
-import { v4 as uuidv4 } from 'uuid'
 
 export class CreateTransactionUseCase {
-    constructor(getUserByIdRepository, createTransactionRepository) {
+    constructor(
+        getUserByIdRepository,
+        createTransactionRepository,
+        idGeneratorAdapter,
+    ) {
         this.getUserByIdRepository = getUserByIdRepository
         this.createTransactionRepository = createTransactionRepository
+        this.idGeneratorAdapter = idGeneratorAdapter
     }
 
     async execute(createTransactionParams) {
@@ -16,7 +20,7 @@ export class CreateTransactionUseCase {
             throw new UserNotFoundError(userId)
         }
 
-        const transactionId = uuidv4()
+        const transactionId = this.idGeneratorAdapter.execute()
 
         const transaction = await this.createTransactionRepository.execute({
             ...createTransactionParams,
