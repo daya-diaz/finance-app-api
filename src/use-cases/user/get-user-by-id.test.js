9fe9a44c5d6244fb72bf762ed3dc7995f8f23b4d
Dayane Santos de Melo <dayadiazprofissional@gmail.com>
2026-02-28 11:03:15 -0300

    test: ensure GetUserByIdUseCase calls GetUserByIdRepository with correct params

--- src/use-cases/user/get-user-by-id.test.js
+++ src/use-cases/user/get-user-by-id.test.js
@@ -52,4 +52,20 @@ describe('GetUserByIdUseCase', () => {
         // assert
         await expect(promise).rejects.toThrow(new UserNotFoundError(userId))
     })
+
+    it('should call GetUserByIdRepository with correct values', async () => {
+        // arrange
+        const { sut, getUserByIdRepository } = makeSut()
+        const userId = faker.string.uuid()
+        const getUserByIdRepositorySpy = jest.spyOn(
+            getUserByIdRepository,
+            'execute',
+        )
+
+        // act
+        await sut.execute(userId)
+
+        // assert
+        expect(getUserByIdRepositorySpy).toHaveBeenCalledWith(userId)
+    })
 })
